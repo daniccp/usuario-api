6b70c70912d85f8cd2cb019ef9dc3edc12ac27f6
Daniele Castro <danicastrodc@yahoo.com.br>
2025-10-22 09:50:32 -0300

    2º commit do projeto
    Signed-off-by: Daniele Castro <danicastrodc@yahoo.com.br>

--- src/main/java/dlp/usuarioapi/entities/Usuario.java
+++ src/main/java/dlp/usuarioapi/entities/Usuario.java
@@ -7,14 +7,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 
 import java.time.LocalDate;
 
 @Entity
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class Usuario {
 
